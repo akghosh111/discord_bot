ed9ef496798fe4be21ce63dcd3f094d162720644
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-26 20:20:24 +0530

    feat: added interaction

--- index.js
+++ index.js
@@ -90,3 +90,27 @@ client.once(Events.ClientReady, async () => {
     console.log(`Activity set to ${activityType} ${activityName}`)
 });
 
+
+client.on(Event.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if(!command) {
+        // console.error(`No commands matching ${interaction.commandName} was found`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true});
+        } else {
+            await interaction.reply({content: "There was an error while executing this command", ephemeral: true});
+        }
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
