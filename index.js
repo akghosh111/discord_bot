5556112347a5c1604b4285952ae6a278fbc986e7
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-26 20:05:09 +0530

    feat: statusType & activityType added

--- index.js
+++ index.js
@@ -21,5 +21,72 @@ const client = new Client({
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message,
+        Partials.User,
+        Partials.GuildMember
     ]
-})
+});
+
+client.commands = new Collection();
+
+const fs = require("fs");
+const path = require("path");
+
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`The command ${filePath} is missing a required "data" or "execute" property.`)
+    }
+
+}
+
+client.once(Events.ClientReady, async () => {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+
+
+    await deployCommands();
+    console.log(`Commands deployed globally`)
+
+
+    const statusType = process.env.BOT_STATUS || "online";
+    const activityType = process.env.ACTIVITY_TYPE || "PLAYING";
+    const activityName = process.env.ACTIVITY_NAME || "Discord";
+
+    const activityTypeMap = {
+        "PLAYING": ActivityType.Playing,
+        "WATCHING": ActivityType.Watching,
+        "LISTENING": ActivityType.Listening,
+        "STREAMING": ActivityType.Streaming,
+        "COMPETING": ActivityType.Competing
+    };
+
+    const statusMap = {
+        "online": PresenceUpdateStatus.Online,
+        "idle": PresenceUpdateStatus.Idle,
+        "dnd": PresenceUpdateStatus.DoNotDisturb,
+        "invisible": PresenceUpdateStatus.Invisible
+    };
+
+    client.user.setPresence({
+        status: statusMap[statusType],
+        activities: [{
+            name: activityName,
+            type: activityTypeMap[activityType]
+        }]
+    });
+
+    console.log(`Bot status set to ${statusType}`);
+    console.log(`Activity set to ${activityType} ${activityName}`)
+});
+
